348241eafdd1bc9b6e1b5c6dd3ec825516a108dd
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-14 19:01:01 +0100

    Update main.py
    fix con extension Autodocstring

--- demo_sonarqube_project/src/main.py
+++ demo_sonarqube_project/src/main.py
@@ -6,11 +6,23 @@ app = FastAPI(title=APP_NAME)
 
 
 def sumar(a: int, b: int) -> int:
-    
+    """Suma dos variables que entren por parametros
+
+    Args:
+        a (int): primer numero
+        b (int): segundo numero
+
+    Returns:
+        int: Devuelve la suma de ambos
+    """
     return a + b
 
 
 @app.get("/saludo")
 def saludo() -> dict:
-    
+    """Crea un saludo en JSON con el mensaje que queramos
+
+    Returns:
+        dict: devuelve un objeto JSON con el mensaje en particular
+    """
     return {"msg": "Hola, SonarQube!"}
